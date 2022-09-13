0ac96311ab390442e229d11d8250bc306416d9f3
GuiFMoura <112089708+GuiFMoura@users.noreply.github.com>
2022-09-13 10:34:07 -0400

    Update Program.java

--- product/src/application/Program.java
+++ product/src/application/Program.java
@@ -8,19 +8,27 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		Product product = new Product();
+
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
-		product.name = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.print("Price: ");
-		product.price = sc.nextDouble();
+		double price = sc.nextDouble();
 		System.out.print("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		int quantity = sc.nextInt();
+		
+		Product product = new Product(name, price, quantity);
+		Product product2 = new Product(name, price);
+		
+		System.out.println(product.getName());
+		System.out.println(product.getPrice());
+		System.out.println(product.getQuantity());
+		
 		System.out.println();
 		System.out.println("Product data: " + product);
 		System.out.println();
 		System.out.print("Enter the number of products to be added in stock: ");
-		int quantity = sc.nextInt();
+		quantity = sc.nextInt();
 		product.addProducts(quantity);
 		System.out.println();
 		System.out.println("Updated data: " + product);
